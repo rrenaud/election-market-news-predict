eb82064d92b0f183fdae3fb0d9a1e3b1f0cab14b
Rob Renaud <rrenaud@gmail.com>
2011-12-15 02:12:35 -0500

    use only w3m to textualize html get rid of unused code from stackoverflow

--- textualize_html.py
+++ textualize_html.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python
 
-from BeautifulSoup import BeautifulSoup
-
 import os
-
-# http://stackoverflow.com/questions/3593784/html-to-text-conversion-using-python-language
-def gettextonly(soup):
-    v = soup.string
-    if v == None:
-        c = soup.contents
-        resulttext=''
-        for t in c:
-            subtext = gettextonly(t)
-            resulttext += subtext + ' '
-        return resulttext
-    else:
-        return v.strip()    
 
 def main():
   existing_output_list = set(os.listdir('text_results/'))
