b5a869841dda40c7abfdf73386c983e97652dc28
Rob Renaud <rrenaud@gmail.com>
2011-12-15 02:10:57 -0500

    now using timeouts

--- fetch_search_links.py
+++ fetch_search_links.py
@@ -3,26 +3,35 @@
 import simplejson as json
 import os
 import pprint
-import urllib
+import urllib2
 import sha
 
 def main():
+    existing_results = set(os.listdir('results/'))
     for fn in os.listdir('searches/'):
-        contents = json.load(open('searches/' + fn))
         try:
+            contents = json.load(open('searches/' + fn))
             results = contents['bossresponse']['news']['results']
         except KeyError, e:
+            print 'skipping', fn
+            continue
+        except json.decoder.JSONDecodeError, e:
             print 'skipping', fn
             continue
         date = fn[-8:]
         for result in results:
             url = result['url']
-            out_fn = 'results/' + date + '.' + sha.sha(url).hexdigest()
-            if os.path.exists(out_fn):
+            out_fn = date + '.' + sha.sha(url).hexdigest()
+            if out_fn in existing_results:
                 print 'skipping', url
             else:
                 print 'getting', url
-                open(out_fn, 'w').write(urllib.urlopen(url).read())
+                try:
+                    open('results/' + out_fn, 'w').write(
+                        urllib2.urlopen(url, timeout=20).read())
+                    existing_results.add(out_fn)
+                except Exception, e:
+                    print 'skipping', url, 'because', e
 
 if __name__ == '__main__':
     main()
